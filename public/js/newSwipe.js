a75f0425078d29cd9588ff38be59754af36f197b
Kitamirike Rogers <38127265+kitaroghope@users.noreply.github.com>
2025-03-06 13:59:52 +0300

    Added some funcitonality

--- public/js/newSwipe.js
+++ public/js/newSwipe.js
@@ -1,6 +1,24 @@
 function nextSong(){
+    // console.log(nextR)
     loadSong(nextR.replace('/',"").replace(".html",""));
 }
 function prevSong(){
+    // console.log(prevL)
     loadSong(prevL.replace('/',"").replace(".html",""));
+}
+
+function changeLanguage(){
+    // Retrieve the current language from localStorage or default to 'english'
+    let currentLanguage = localStorage.getItem('language') || 'english';
+
+    // Toggle the language between 'english' and 'luganda'
+    currentLanguage = currentLanguage === 'english' ? 'luganda' : 'english';
+
+    // Update the button text
+    $('.lang').text(currentLanguage == 'english' ? 'luganda' : 'english');
+
+    // Save the updated language to localStorage
+    localStorage.setItem('language', currentLanguage);
+    loadSong(hymnal);
+
 }
